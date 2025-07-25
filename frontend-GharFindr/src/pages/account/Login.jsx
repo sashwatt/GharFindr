bf79c1f1146e651a0810e7d37a72de1c0da285ba
sashwatt <supersashwat@gmail.com>
2025-07-25 18:33:17 +0545

    Strong password policy added

--- frontend-GharFindr/src/pages/account/Login.jsx
+++ frontend-GharFindr/src/pages/account/Login.jsx
@@ -3,7 +3,7 @@ import { FaEnvelope, FaLock, FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import logo from "../../assets/icons/gharfindr.png"; // Adjust path if needed
+import logo from "../../assets/icons/gharfindr.png";
 import axios from "axios";
 
 const Login = () => {
