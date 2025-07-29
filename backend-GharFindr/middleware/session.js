653b54155ca1a652287eb193e09162b7731bab9f
sashwatt <supersashwat@gmail.com>
2025-07-29 14:04:21 +0545

    session added in mongodb

--- backend-GharFindr/middleware/session.js
+++ backend-GharFindr/middleware/session.js
@@ -14,7 +14,7 @@ const sessionMiddleware = session({
     autoRemove: 'native'
   }),
   cookie: {
-    maxAge: 1000 * 10, // 30 minutes
+    maxAge: 1000 * 60 * 30, // 30 minutes
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production', // true in production (HTTPS)
     sameSite: 'strict'
